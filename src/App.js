36ea48b264d77c5ceef31efdf352ff4445ec292e
namaste-code <shitanshu.19p@gmail.com>
2019-05-15 23:16:13 +0530

    Initial Commit: Form created.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {
+  useState, 
+  useEffect
+} from 'react';
+// Libraries
+import axios from 'axios';
+
+// Custome files
 import './App.css';
 
+const axiosGitHubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
+  }
+});
+
+const TITLE = 'React GraphQL GitHub Client';
+
 function App() {
+  const [path, setPath] = useState('the-road-to-learn-react/the-road-to-learn-react');
+
+  const doFetch = () => {
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{TITLE}</h1>
+
+      <form 
+        onSubmit={event => {
+          doFetch();
+          event.preventDefault();
+        }}
+      >
+        <label htmlFor="url">
+          Show open issues for https://github.com/
+        </label>
+        <input
+          id="url"
+          type="text"
+          value={path}
+          onChange={event => setPath(event.target.value)}
+          style={{ width: '300px' }}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      <hr />
+
+      {/** Here comes the result. */}
     </div>
   );
 }
