ffd3d3d5602c97f20dc528f929dc1a44394f7a19
namaste-code <shitanshu.19p@gmail.com>
2019-05-16 00:17:28 +0530

    GraphQL Query using HTTP POST request and payload.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 
 // Custome files
 import './App.css';
+import Organization from './components/Organization';
 
 const axiosGitHubGraphQL = axios.create({
   baseURL: 'https://api.github.com/graphql',
@@ -31,13 +32,18 @@ const GET_ORGANIZATION = `
 
 function App() {
   const [path, setPath] = useState('the-road-to-learn-react/the-road-to-learn-react');
+  const [organization, setOrganization] = useState(null);
+  const [errors, setErrors] = useState(null);
 
   useEffect(() => {
     console.log('Using effect to fecth data.');
     const fetchData = async () => {
       axiosGitHubGraphQL
       .post('', { query: GET_ORGANIZATION })
-      .then(result => console.log(result));
+      .then(result => {
+        setOrganization(result.data.data.organization);
+        setErrors(result.data.data.errors);
+      });
     };
 
     fetchData();
@@ -69,7 +75,17 @@ function App() {
 
       <hr />
 
-      {/** Here comes the result. */}
+      {
+        organization ? (
+          <Organization 
+            organization={organization} 
+            errors={errors}
+          />
+        ) : (
+          <p>Loading ...</p>
+        )
+      }
+      
     </div>
   );
 }
---
+++ src/components/Organization.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Organization({ organization, errors }) {
+    
+    if (errors) {
+        return (
+            <p>
+                <strong>Something went wrong: </strong>
+                {errors.map(error => error.message).join(' ')}
+            </p>
+        );
+    }
+
+    return (
+        <div>
+            <p>
+                <strong>Issues from Organization: </strong>
+                <a href={organization.url}>{organization.name}</a>
+            </p>
+        </div>
+    );
+}
+
+export default Organization;
